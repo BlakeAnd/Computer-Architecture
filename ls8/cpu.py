8696290b6b07a217b597dde17eb62e1cc759780c
BlakeAnd <mblakeandrew@gmail.com>
2019-11-12 13:11:36 -0800

    got printing and multpiplying functions and file reading and ram and registers up and working

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,28 +7,44 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.register = [0] * 8
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
-
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
+
+        program_name = sys.argv[1]
+        address = 0
+
+        with open(program_name) as file:
+            for line in file:
+                line = line.split('#')[0]
+                line = line.strip() #lose whitespace
+
+                if line == '':
+                    continue
+
+                val = int(line, 2) #LS-8 uses base 
+                # print(val)
+
+                self.ram[address] = val
+                address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -60,6 +76,53 @@ class CPU:
 
         print()
 
+    def LDI(self, register_num, value):
+        self.register[register_num] = value
+    
+    def PRN(self, register_num):
+        print(self.register[register_num])
+
+    def MULT(self, reg1, reg2):
+        val1 = self.register[reg1]
+        val2 = self.register[reg2]
+        product = val1*val2
+        self.register[reg1] = product
+
+
+    def ram_read(self, address):
+        value = self.ram[address]
+        return value
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
     def run(self):
         """Run the CPU."""
-        pass
+        address = 0
+        HALT = 1
+        LDI = 130
+        PRN = 71
+        MULT = 162
+        IR = self.ram_read(address)
+
+        while IR != HALT:
+            IR = self.ram_read(address)
+            if IR == LDI:
+                register = self.ram_read(address+1)
+                value = self.ram_read(address+2)
+                self.LDI(register, value)
+                address += 3
+            elif IR == PRN:
+                prn_register = self.ram_read(address+1)
+                self.PRN(prn_register)
+                address += 2
+            elif IR == MULT:
+                mult_reg1 = self.ram_read(address+1)
+                mult_reg2 = self.ram_read(address+2)
+                self.MULT(mult_reg1, mult_reg2)
+                address += 3
+
+            # print(address, IR)
+            
+
+
